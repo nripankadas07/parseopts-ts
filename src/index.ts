aa5a72e012e11ebbe8f2c7d55aba2127bddcfc1c
Dr. Nripanka Das <nripankadas@gmail.com>
2026-04-25 15:01:19 +0400

    Add src/index.ts

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+export { parseArgs } from "./parse";
+export { ParseError } from "./errors";
+export type { ParseErrorCode } from "./errors";
+export { defineSchema } from "./types";
+export type {
+  OptionDef,
+  OptionType,
+  OptionValue,
+  ParseResult,
+  Schema,
+} from "./types";
